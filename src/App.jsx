ff4ac35b97cc380182d55d8d2b4add9957bdb980
Mauricio-Alonso <conta.alonso@gmail.com>
2022-09-22 16:55:17 -0300

    Agregamos react-bootstrap.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   return (
     <div>
-      
+      <h1 className="diplay-1">Prueba</h1>  
     </div>
   );
 };
